6b20414ad9fc764b3cfb72d75d119e9b1973d908
zippykitche <zippyk80@gmail.com>
2025-07-13 23:16:49 +0300

    removed deprecated code

--- server/server.js
+++ server/server.js
@@ -14,12 +14,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log("✅ MongoDB connected"))
-.catch((err) => console.error("❌ MongoDB connection error:", err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
 
 app.use("/project", projectRoutes);
 app.use("/jobs", jobsRoutes);
