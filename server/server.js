df475cf9b6706f43d54c16985bf0ccd8986377f2
zippykitche <zippyk80@gmail.com>
2025-07-13 23:08:00 +0300

    add mongo db atlas

--- server/server.js
+++ server/server.js
@@ -14,10 +14,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error(err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("✅ MongoDB connected"))
+.catch((err) => console.error("❌ MongoDB connection error:", err));
 
 app.use("/project", projectRoutes);
 app.use("/jobs", jobsRoutes);
